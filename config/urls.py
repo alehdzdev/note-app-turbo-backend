bdd99191bc95fea55f7746e814ea6e3980fc4993
Ale Hernandez <alehdzdev@gmail.com>
2026-03-07 22:35:53 -0400

    [ADD] health endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,16 @@
 # Django
 from django.contrib import admin
 from django.urls import include, path
+from django.http import JsonResponse
+
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("users.urls")),
     path("api/v1/notes/", include("notes.urls")),
+    path("health/", health),
 ]
